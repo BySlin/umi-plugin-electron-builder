adc0c1208faf1dd76f5a5065d8a0f642b524199e
郭亮 <2292735470@qq.com>
2022-12-07 23:34:21 +0800

    chore: 4.0.0-beta6

--- lib/compile/webpack/index.d.ts
+++ lib/compile/webpack/index.d.ts
@@ -1,5 +1,5 @@
 import { IApi } from 'umi';
 import webpack from '@umijs/bundler-webpack/compiled/webpack';
-export declare function getMainWebpackConfig(api: IApi): Configuration;
+export declare function getMainWebpackConfig(api: IApi): any;
 export declare function getPreloadWebpackConfig(api: IApi, inputFileName: string, outputFileName: string): webpack.Configuration;
 export declare const build: (config: webpack.Configuration) => Promise<void>;
